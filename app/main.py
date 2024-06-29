edfa8ad06fd4f3fe299b2450ce57ef67317f3ba0
agentelement <agentelement@protonmail.com>
2024-06-29 12:20:26 -0700

    feat: 2/3gram models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,89 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from app.trigram import TrigramLanguageModel
+from app.bigram import BigramLanguageModel
+
+torch.manual_seed(1337)
 
 
 def main():
-    pass
+    with open("input.txt") as input:
+        text = input.read()
+    chars = sorted(list(set(text)))
+
+    stoi = { ch:i for i,ch in enumerate(chars) }
+    itos = { i:ch for i,ch in enumerate(chars) }
+    encode = lambda s: [stoi[c] for c in s]
+    decode = lambda l: ''.join([itos[i] for i in l])
+
+    data = torch.tensor(encode(text), dtype=torch.long)
+
+    n = int(0.9 * len(data))
+    train_data = data[:n]
+    val_data = data[n:]
+
+    block_size = 8
+
+    x = train_data[:block_size]
+    y = train_data[1:block_size+1]
+
+    batch_size = 4
+
+    xb, yb = get_batch(train_data, block_size, batch_size)
+    
+    print("Bigram")
+
+    m = BigramLanguageModel(len(chars))
+    out = m(xb, yb)
+
+    idx = torch.zeros((1, 1), dtype=torch.long)
+    optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+    batch_size = 32
+    for steps in range(40000):
+        xb, yb = get_batch(train_data, block_size, batch_size)
+        logits, loss = m(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    print(loss.item())
+    
+    print(decode(m.generate(idx, max_new_tokens=1000)[0].tolist()))
+
+    print("Trigram")
+
+    m = TrigramLanguageModel(len(chars))
+    out = m(xb, yb)
+
+    idx = torch.zeros((1, 1), dtype=torch.long)
+    optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+    batch_size = 32
+    for steps in range(40000):
+        xb, yb = get_batch(train_data, block_size, batch_size)
+        logits, loss = m(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    print(loss.item())
+    
+    print(decode(m.generate(idx, max_new_tokens=1000)[0].tolist()))
+
+
+
+def get_batch(data, block_size, batch_size):
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+def esitmate_loss(model):
+    out = {}
+    model.eval()
 
 
 if __name__ == '__main__':
---
+++ app/trigram.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class TrigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size * (vocab_size + 1), 
+                                                  vocab_size)
+        self.vocab_size = vocab_size
+
+    def forward(self, inputs, targets=None):
+        x, y = inputs.shape
+        padding = torch.zeros(x, 1, dtype=torch.long)
+        offset_inputs = torch.cat((padding, inputs[:, 0:y-1]), dim=1)
+        idx = (inputs + 1) * self.vocab_size + offset_inputs
+        logits = self.token_embedding_table(inputs)
+
+        if targets is None:
+            return logits, None
+
+        B, T, C = logits.shape
+        logits = logits.view(B * T, C)
+        targets = targets.view(B * T)
+        loss = F.cross_entropy(logits, targets)
+        return logits, loss
+
+    @torch.no_grad
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
